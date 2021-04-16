7c05c1275de6e5ef048fb489d7b921b12ef649fd
Jose Flavio <jflavio90@gmail.com>
2021-04-15 20:21:55 -0500

    add SpaceXApi

---
+++ shared/src/commonMain/kotlin/com/jflavio/kmm/shared/network/SpaceXApi.kt
@@ -0,0 +1,31 @@
+package com.jflavio.kmm.shared.network
+
+import com.jflavio.kmmtest.RocketLaunch
+import io.ktor.client.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.request.*
+
+/**
+ * SpaceXApi
+ *
+ * @author Jose Flavio - jflavio90@gmail.com
+ * @since  15/04/2021
+ */
+class SpaceXApi {
+
+    private val httpClient = HttpClient {
+        install(JsonFeature) {
+            val json = kotlinx.serialization.json.Json { ignoreUnknownKeys = true }
+            serializer = KotlinxSerializer(json)
+        }
+    }
+
+    suspend fun getAllLaunches(): List<RocketLaunch> {
+        return httpClient.get(LAUNCHES_ENDPOINT)
+    }
+
+    companion object {
+        private const val LAUNCHES_ENDPOINT = "https://api.spacexdata.com/v3/launches"
+    }
+}
