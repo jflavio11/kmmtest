84bb450a536ac6db97e1792ae3db9981a1142611
Jose Flavio <jflavio90@gmail.com>
2021-04-15 19:51:36 -0500

    add database class and querys for shared module

---
+++ shared/src/commonMain/kotlin/com/jflavio/kmm/shared/cache/Database.kt
@@ -0,0 +1,95 @@
+package com.jflavio.kmm.shared.cache
+
+import com.jflavio.kmmtest.Links
+import com.jflavio.kmmtest.Rocket
+import com.jflavio.kmmtest.RocketLaunch
+
+/**
+ * Database
+ *
+ * @author Jose Flavio - jflavio90@gmail.com
+ * @since  15/04/2021
+ */
+internal class Database(private val databaseDriverFactory: DatabaseDriverFactory) {
+    private val database = KmmDatabase(databaseDriverFactory.createDriver())
+    private val dbQuery = database.kmmDatabaseQueries
+
+    internal fun clearDatabase() {
+        dbQuery.transaction {
+            dbQuery.removeAllLaunches()
+            dbQuery.removeAllRockets()
+        }
+    }
+
+    internal fun getAllLaunches(): List<RocketLaunch> {
+        return dbQuery.selectAllLaunchesInfo(::mapLaunchSelecting).executeAsList()
+    }
+
+    internal fun createLaunches(launches: List<RocketLaunch>) {
+        dbQuery.transaction {
+            launches.forEach { launch ->
+                val rocket = dbQuery.selectRocketById(launch.rocket.id).executeAsOneOrNull()
+                if (rocket == null) {
+                    insertRocket(launch)
+                }
+
+                insertLaunch(launch)
+            }
+        }
+    }
+
+    private fun insertRocket(launch: RocketLaunch) {
+        dbQuery.insertRocket(
+            id = launch.rocket.id,
+            name = launch.rocket.name,
+            type = launch.rocket.type
+        )
+    }
+
+    private fun insertLaunch(launch: RocketLaunch) {
+        dbQuery.insertLaunch(
+            flightNumber = launch.flightNumber.toLong(),
+            missionName = launch.missionName,
+            launchYear = launch.launchYear,
+            rocketId = launch.rocket.id,
+            details = launch.details,
+            launchSuccess = launch.launchSuccess ?: false,
+            launchDateUTC = launch.launchDateUTC,
+            missionPatchUrl = launch.links.missionPatchUrl,
+            articleUrl = launch.links.articleUrl
+        )
+    }
+
+    private fun mapLaunchSelecting(
+        flightNumber: Long,
+        missionName: String,
+        launchYear: Int,
+        rocketId: String,
+        details: String?,
+        launchSuccess: Boolean?,
+        launchDateUTC: String,
+        missionPatchUrl: String?,
+        articleUrl: String?,
+        rocket_id: String?,
+        name: String?,
+        type: String?
+    ): RocketLaunch {
+        return RocketLaunch(
+            flightNumber = flightNumber.toInt(),
+            missionName = missionName,
+            launchYear = launchYear,
+            details = details,
+            launchDateUTC = launchDateUTC,
+            launchSuccess = launchSuccess,
+            rocket = Rocket(
+                id = rocketId,
+                name = name!!,
+                type = type!!
+            ),
+            links = Links(
+                missionPatchUrl = missionPatchUrl,
+                articleUrl = articleUrl
+            )
+        )
+    }
+}
