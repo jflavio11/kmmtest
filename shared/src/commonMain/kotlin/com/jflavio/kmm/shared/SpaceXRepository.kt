944d9f8abcda074208954b3adcf4ca58bcc7cac2
Jose Flavio <jflavio90@gmail.com>
2021-04-15 20:39:37 -0500

    add SpaceXRepository

---
+++ shared/src/commonMain/kotlin/com/jflavio/kmm/shared/SpaceXRepository.kt
@@ -0,0 +1,41 @@
+package com.jflavio.kmm.shared
+
+import com.jflavio.kmm.shared.cache.Database
+import com.jflavio.kmm.shared.cache.DatabaseDriverFactory
+import com.jflavio.kmm.shared.network.SpaceXApi
+import com.jflavio.kmmtest.RocketLaunch
+
+/**
+ * SpaceXSDK
+ *
+ * @author Jose Flavio - jflavio90@gmail.com
+ * @since  15/04/2021
+ */
+class SpaceXRepository(private val databaseDriverFactory: DatabaseDriverFactory) {
+    private val database = Database(databaseDriverFactory)
+    private val api = SpaceXApi()
+
+    /**
+     * Get all the rocket launches from cache or from the SpaceX API.
+     *
+     * To handle exceptions produced by the Ktor client, in Swift, we
+     * need to mark our function with @Throws annotation. All Kotlin
+     * exceptions are unchecked, while Swift has only checked errors.
+     * Thus, to make Swift code aware of expected exceptions, Kotlin
+     * functions should be marked with an @Throws annotation specifying
+     * a list of potential exception classes.
+     */
+    @Throws(Exception::class)
+    suspend fun getLaunches(forceReload: Boolean): List<RocketLaunch> {
+        val cachedLaunches = database.getAllLaunches()
+        return if (cachedLaunches.isNotEmpty() && !forceReload) {
+            cachedLaunches
+        } else {
+            api.getAllLaunches().also {
+                database.clearDatabase()
+                database.createLaunches(it)
+            }
+        }
+    }
+
+}
